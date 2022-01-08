b198d9962a06518c04bba1edee9d43b7f13b29d2
Timo Triisa <timotriisa@gmail.com>
2022-01-08 10:35:58 +0200

    Lisasin kommentaarid

--- server.js
+++ server.js
@@ -1,36 +1,47 @@
-var fs = require("fs");
-const http = require('http');
+var fs = require("fs");         // Impordib node file-system library
+const http = require('http');   // require abil saab koodi lisada ka enda teisi JS faile require("./minufail.js")
 
+// Olek (state) või andmebaas
 let fruits = ["apple","pear","banana","bamboo","ananass","arbuus"]
 
-// http://localhost:8080/?q=pirn
+/**
+ * See funktsioon käivitub iga kord kui keegi kodulehte külastab
+ * @param {*} req Request sees on kirjeldatud kasutaja soove
+ * @param {*} res Response sellega saab saata kasutajale tagasi vastuse
+ */
 function requestListener(req, res) {
-    console.log(req.url)
+    console.log(req.url) // Prindib käsurea aknasse mis aadressi kasutaja brauseris avas
     let otsingusõna = ""
     let filterResults
 
-    console.log(req.url.includes("otsi"))
+    // Kui meil on aadressi peal otsing siis proovime otsingusõna aadressist eraldada
     if (req.url.includes("otsi"))
         otsingusõna = req.url.split("=")[1]
-    if (otsingusõna == "")
+    
+    // Kui otsingusõna ei leitud siis näitame kasutajale kõiki tulemusi
+    if (otsingusõna == "") {
         filterResults = fruits
-    else
+    } else {
+        // Teeme fruits arrayst koopia milles on ainult puuviljad mis algavad otsingusõnaga
         filterResults = fruits.filter(fruit => fruit.startsWith(otsingusõna))
+    }
 
-    console.log("otsingusõna", otsingusõna, filterResults)
-
+    // Paneme kokku HTML vastuse
     let html = "<html><head><title>Test</title></head><body>"
     html += "<form><input name='otsi' /><button>Filtreeri</button></form>"
     filterResults.forEach(fruit => html += fruit+"<br>")
     /*
-    for (let i=0; i<fruits.length; i++)
-        html += fruits[i]
+    for (let i=0; i<fruits.length; i++) {
+        let fruit = fruits[i]
+        html += fruit+"<br>"
+    }
     */
     
     html += "</body></html>"
 
-    res.writeHead(200);
-    res.end(html);
+    // Saadame HTMLi tagasi kasutajale
+    res.writeHead(200); // Ütleb brauserile, et päringu täitmine läks plaanipäraselt
+    res.end(html); // Saadab kirja teele
 }
 
 const server = http.createServer(requestListener);
